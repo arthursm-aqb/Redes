9d7ffbff10decd63bfefc16d0b8be3039d8f52ce
Arthur Mariz <arthur.mariz_silva@hotmail.com>
2025-12-27 19:51:00 -0300

    Interface gráfica para mostrar os clientes no servidorTCP

--- interface.py
+++ interface.py
@@ -23,3 +23,44 @@ class Dashboard:
             print(f"{ip:<20} | {dados['so']}")
 
         print("-" * 50)
+
+    def detalharCliente(self, ip, dados):
+        self.clean()
+        if dados is None:
+            print(f"Erro: Cliente com IP {ip} não encontrado.")
+            input("Pressione ENTER para voltar...")
+            return
+
+        print("=" * 60)
+        print(f"Detalhes do cliente: {ip}")
+        print("=" * 60)
+
+        print(f"Sistema Operacional: {dados['so']}")
+
+        cpu = dados['processador']
+        ram = dados['memoria']
+        disco = dados['disco']
+
+        print("-" * 60)
+        print(f"CPU: {cpu['fisico']} Núcleos Físicos / {cpu['logico']} Lógicos (Uso: {cpu['uso']}%)")
+        print(f"RAM: {ram['livre']} GB Livres / {ram['total']} GB Total")
+        print(f"Disco: {disco['livre']} GB Livres / {disco['total']} GB Total")
+
+        print("-" * 60)
+        print("Interfaces de rede:")
+        print(f"{'NOME':<30} | {'TIPO':<10} | {'STATUS'}")
+
+        for item in dados['rede']:
+            nome = item['nome_interface']
+            tipo = item['tipo_interface']
+            status = item['status_interface']
+
+            print(f"{nome[:30]:<30} | {tipo:<10} | {status}")
+
+            print(f"   -> IPv4: {item['ipv4']}")
+            print(f"   -> IPv6: {item['ipv6']}")
+            print(f"   -> MAC:  {item['mac']}")
+            print("." * 60)
+
+        print("=" * 60)
+        input("Pressione ENTER para voltar ao Dashboard...")
--- servidorTCP.py
+++ servidorTCP.py
@@ -1,5 +1,6 @@
 import socket
 import json
+from interface import Dashboard
 
 porta = 6000
 
@@ -8,39 +9,69 @@ class servidorTCP:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cria um ponto de conexão IPv4 TCP
         self.socket.bind(('0.0.0.0', porta)) # Vincula esse ponto de conexão na porta 6000 e aceita receber conexões de qualquer interface de rede (Wi-Fi, Ethernet, Loopback)
         self.socket.listen() # Começa a esperar solicitações de conexões TCP IPv4
+        self.socket.settimeout(1.0)
+        self.clientes = {}
+        self.tela = Dashboard()
         print(f"Ponto de conexão do servidor inicializado com sucesso no protocolo TCP!")
 
     def listen(self):
-        print(f"Aguardando conexão TCP...")
+        print(f"Aguardando conexão TCP... (Pressione CTRL+C para acessar o menu!)")
         while True:
-            conexao, endereco = self.socket.accept() # Aceita a solicitação de conexão TCP e atribui a conexao um ponto de conexão TCP IPv4 temporário entre servidor-cliente e endereco um endereço IPv4 do cliente
-            print(f"Conexão TCP feita com sucesso com: {endereco[0]}")
-            self.processar_dados_cliente(conexao, endereco) # Passa o ponto de conexão com o cliente e o endereço IPv4 dele a uma função auxiliar que irá processar e imprimir os dados relacionados ao computador-cliente
+            try:
+                while True:
+                    try:
+                        conexao, endereco = self.socket.accept() # Aceita a solicitação de conexão TCP e atribui a conexao um ponto de conexão TCP IPv4 temporário entre servidor-cliente e endereco um endereço IPv4 do cliente
+                        print(f"Conexão TCP feita com sucesso com: {endereco[0]}")
+                        self.processar_dados_cliente(conexao, endereco) # Passa o ponto de conexão com o cliente e o endereço IPv4 dele a uma função auxiliar que irá processar e imprimir os dados relacionados ao computador-cliente
+                    except socket.timeout:
+                        pass
+            except KeyboardInterrupt:
+                self.tela.clean()
+                print("--- MENU DO SERVIDOR ---")
+                print("1. Detalhar um cliente")
+                print("2. Voltar ao monitoramento")
+                print("0. Sair")
+
+                opcao = input("Escolha: ")
+                if opcao == "1":
+
+                    self.tela.clean()
+                    print("Clientes disponíveis :D :")
+                    print("-" * 30)
+                    if len(self.clientes) == 0:
+                        print("Nenhum cliente conectado ainda ;(")
+                        input("Pressione ENTER para voltar ao monitoramento...")
+                        self.tela.desenharDashboard(self.clientes)
+                        continue
+
+                    for ips in self.clientes:
+                        print(f"-> {ips}")
+                    print("-" * 30)
+
+                    ip_alvo = input("Digite o IP do cliente: ")
+                    dados_cliente = self.clientes.get(ip_alvo)
+                    self.tela.detalharCliente(ip_alvo, dados_cliente)
+                    self.tela.desenharDashboard(self.clientes)
+
+                elif opcao == "2":
+
+                    print("Retornando...")
+                    self.tela.desenharDashboard(self.clientes)
+                    continue
+
+                elif opcao == "0":
+                    print("Encerrando servidor...")
+                    break  # Quebra o loop e fecha o programa
+
 
     def processar_dados_cliente(self, conexao, endereco):
         try:
             dados = conexao.recv(4096) # Lê até os 4096 bytes do fluxo de dados enviado ao ponto de conexão TCP IPv4.
             if len(dados)>0: # Executa se o pacote não estiver vazio
                 relatorio = json.loads(dados.decode('utf-8')) # Decodifica o pacote em string no formato utf-8 e é transformado em dicionário contendo os dados pelo json.
-
-                # Impressão dos dados do SO, CPU, RAM E HD/SSD do Cliente
-                print(f"\n" + "-" * 30)
-                print(f"Dados do Cliente: {endereco[0]}")
-                print(f"Sistema Operacional: {relatorio['so']}")
-                print(f"CPU: {relatorio['processador']['fisico']} núcleos fisicos, {relatorio['processador']['logico']} núcleos logicos, uso do processador {relatorio['processador']['uso']}%")
-                print(f"RAM total: {relatorio['memoria']['total']} GB, RAM livre: {relatorio['memoria']['livre']} GB")
-                print(f"Disco total: {relatorio['disco']['total']} GB, Disco livre: {relatorio['disco']['livre']} GB")
-                print("-" * 20)
-
-                for item in relatorio['rede']:
-                    # Aqui usamos as chaves exatas que você definiu no dicionário info_interface
-                    print(f"Interface: {item['nome_interface']} ({item['tipo_interface']})")
-                    print(f"IPv4: {item['ipv4']}")
-                    print(f"IPv6: {item['ipv6']}")
-                    print(f"MAC:  {item['mac']}")
-                    print(f"Status: {item['status_interface']}")
-                    print("." * 20)
-                print(f"\n" + "-" * 30)
+                ip_cliente = endereco[0]
+                self.clientes[ip_cliente] = relatorio
+                self.tela.desenharDashboard(self.clientes)
 
         except Exception as e: # Caso ocorra algum erro, o imprime no terminal
             print(f"Erro no processamento de dados do cliente: {e} ")
