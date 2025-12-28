9632f7632eb057fa8a31391fc52661d9b0f6b817
Arthur Mariz <arthur.mariz_silva@hotmail.com>
2025-12-28 13:43:36 -0300

    Comentários

--- servidorUDP.py
+++ servidorUDP.py
@@ -16,16 +16,16 @@ class servidorUDP:
         print(f"Aguardando broadcast...")
         while True:
             try:
-                mensagem_criptografada, endereco = self.socket.recvfrom(4096) # Espera o envio de um pacote contendo os dadosCliente e o endereço de origem. Lê até 4096 bytes e ignora o resto.
+                mensagem_criptografada, endereco = self.socket.recvfrom(4096) # Espera o envio de um pacote criptografado contendo a identificação do cliente "HELLO" e seu endereço de origem. Lê até 4096 bytes e ignora o resto.
                 try:
-                    mensagem_decodificada = cipher.decrypt(mensagem_criptografada) # Decodifica os bytes em uma String
+                    mensagem_decodificada = cipher.decrypt(mensagem_criptografada) # Descriptografa os bytes
 
-                    if mensagem_decodificada.decode('utf-8') == 'HELLO': # Se a mensagem for uma String "HELLO" captura o endereço do cliente e envia ao cliente um pacote contendo uma mensagem "SUCESSO", caso contrário, espera outro pacote.
+                    if mensagem_decodificada.decode('utf-8') == 'HELLO': # Decodifica os bytes em string e se a mensagem for uma String "HELLO" captura o endereço do cliente e envia ao cliente um pacote contendo uma mensagem "SUCESSO", caso contrário, espera outro pacote.
                         print(f"Cliente de IP {endereco[0]} encontrado")
-                        resposta = "Sucesso".encode('utf-8')
-                        resposta_crypto = cipher.encrypt(resposta)
-                        self.socket.sendto(resposta_crypto, endereco) # Envia ao cliente a mensagem "Sucesso"
-                except Exception as e_crypt:
+                        resposta = "Sucesso".encode('utf-8') # Codifica "Sucesso" em bytes no formato UTF-8
+                        resposta_crypto = cipher.encrypt(resposta) # Criptografa a resposta
+                        self.socket.sendto(resposta_crypto, endereco) # Envia ao cliente a resposta criptografada "Sucesso"
+                except Exception as e_crypt: # Caso ocorra problema de descriptografia, avisa ao usuário.
                     print(f"Pacote não autenticado do endereço: {endereco[0]}")
 
             except Exception as e: # Se acontecer um erro na função listen, imprime na tela o erro
