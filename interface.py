58c73d61ac42d6b18a83ad13ac8cfd80a2995581
Arthur Mariz <arthur.mariz_silva@hotmail.com>
2025-12-28 00:06:39 -0300

    comentários interface

--- interface.py
+++ interface.py
@@ -5,24 +5,47 @@ class Dashboard:
     def __init__(self):
         pass
 
-    def clean(self):
-        if os.name == "nt":
-            os.system("cls")
-        else:
-            os.system("clear")
+    def clean(self): # A função auxiliar que limpa a tela
+        if os.name == "nt": # Verifica se o Sistema Operacional é Windows (nt)
+            os.system("cls") # Executa o comando de shell 'cls' nativo do Windows
+        else: # Caso contrário (Linux/macOS)
+            os.system("clear") # Executa o comando de shell 'clear' (padrão Unix)
 
-    def desenharDashboard(self, listaClientes):
-        self.clean()
+    def desenharDashboard(self, listaClientes): # Função que imprime o dashboard na tela do terminal com os dados do usuário
+        self.clean() # Limpa a tela atual
 
-        print("-" * 50)
+        tempo = time.time() # Recebe o tempo no momento da chamada
+
+        online = 0; # Contador de clientes com status online
+        offline = 0; # Contador de clientes com status offline
+
+        for ip, dados in listaClientes.items(): # ip recebe os ips da listaCliente e dados os valores do dicionário
+            ultimo_visto = dados.get('visibilidade') # Pega o tempo que o ip atual fez uma conexão com o servidor
+
+            diferenca = tempo - ultimo_visto  # Pega a diferença de tempo entre conexão do servidor do cliente e o tempo atual do servidor
+
+            if diferenca < 30: # Se o tempo for menor que 30, então o servidor ainda considera que o cliente está on-line
+                online+=1
+                dados['visibilidade_temp'] = "ONLINE" # Cria uma chave em dados para mostrar que o servidor está on-line
+            else: # caso o tempo exceda 30 segundos, considera que o cliente caiu ou desconectou (Timeout)
+                offline+=1
+                dados['visibilidade_temp'] = "OFFLINE" # Cria um chave em dados para mostrar que o servidor está off-line
+
+        # Imprime no painel o ip dos clientes conectados ao servidor e informações simplificadas
+        print("-" * 75)
         print("PAINEL COM CLIENTES CONECTADOS AO SERVIDOR")
-        print("-" * 50)
-        print(f"{'IP':<20} | {'SISTEMA OPERACIONAL'}")
+        print("-" * 75)
+        print(f"Clientes Online: {online} | Clientes Offline: {offline}")
+        print("-" * 75)
+        print(f"{'IP':<20} | {'SISTEMA OPERACIONAL':<30} | {'STATUS'}")
+        print("-" * 75)
 
         for ip, dados in listaClientes.items():
-            print(f"{ip:<20} | {dados['so']}")
+            status = dados.get('visibilidade_temp', '?')
 
-        print("-" * 50)
+            print(f"{ip:<20} | {dados['so']:<30} | {status}")
+
+        print("-" * 75)
 
     def detalharCliente(self, ip, dados):
         self.clean()
@@ -31,9 +54,9 @@ class Dashboard:
             input("Pressione ENTER para voltar...")
             return
 
-        print("=" * 60)
+        print("=" * 75)
         print(f"Detalhes do cliente: {ip}")
-        print("=" * 60)
+        print("=" * 75)
 
         print(f"Sistema Operacional: {dados['so']}")
 
@@ -41,12 +64,12 @@ class Dashboard:
         ram = dados['memoria']
         disco = dados['disco']
 
-        print("-" * 60)
+        print("-" * 75)
         print(f"CPU: {cpu['fisico']} Núcleos Físicos / {cpu['logico']} Lógicos (Uso: {cpu['uso']}%)")
         print(f"RAM: {ram['livre']} GB Livres / {ram['total']} GB Total")
         print(f"Disco: {disco['livre']} GB Livres / {disco['total']} GB Total")
 
-        print("-" * 60)
+        print("-" * 75)
         print("Interfaces de rede:")
         print(f"{'NOME':<30} | {'TIPO':<10} | {'STATUS'}")
 
@@ -60,7 +83,7 @@ class Dashboard:
             print(f"   -> IPv4: {item['ipv4']}")
             print(f"   -> IPv6: {item['ipv6']}")
             print(f"   -> MAC:  {item['mac']}")
-            print("." * 60)
+            print("." * 75)
 
-        print("=" * 60)
+        print("=" * 75)
         input("Pressione ENTER para voltar ao Dashboard...")
